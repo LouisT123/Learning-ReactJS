8805378e695fb7f1f9d8d0158fa65e5a21d448c4
Spidre <52509987+LouisT123@users.noreply.github.com>
2023-03-26 23:44:31 -0700

    Added styling

--- react-tutorial-1/src/components/ExpenseItem.js
+++ react-tutorial-1/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return (<h2> Expense item here! </h2>);
+  return (
+    <div className="expense-item">
+      <div>April 2nd 2023</div>
+      <div className="expense-item_description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item_price">$500.00</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
