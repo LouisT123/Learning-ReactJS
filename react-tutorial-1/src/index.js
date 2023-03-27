bbb7f80ad778f96b9311751674252a06ac610bed
Spidre <52509987+LouisT123@users.noreply.github.com>
2023-03-26 23:01:51 -0700

    Creating custom component

--- react-tutorial-1/src/index.js
+++ react-tutorial-1/src/index.js
@@ -1,7 +1,11 @@
+//import react dom dependency
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
 
+//create root: creates main entry point
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+
