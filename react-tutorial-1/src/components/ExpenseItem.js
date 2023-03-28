ee834ad096809f132a08a4f559a835979d22e7e4
Spidre <louistaing@aol.com>
2023-03-28 14:12:52 -0700

    Display date into human readable

--- react-tutorial-1/src/components/ExpenseItem.js
+++ react-tutorial-1/src/components/ExpenseItem.js
@@ -2,11 +2,18 @@ import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
 
+  const month = props.date1.toLocaleString('en-US', { month: 'long'});
+  const day = props.date1.toLocaleString('en-US', { day: '2-digit'});
+  const year = props.date1.getFullYear();
   return (
      /* className for css styling*/
     <div className="expense-item">
         {/* print date object*/}
-      <div>{props.date1.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
         {/* print title and amount */}
       <div className="expense-item_description">
         <h2>{props.title1}</h2>
