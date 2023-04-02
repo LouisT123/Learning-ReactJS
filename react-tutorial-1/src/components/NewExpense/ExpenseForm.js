c09921d9229d2e2491abaff687aa77a34fc81106
Spidre <52509987+LouisT123@users.noreply.github.com>
2023-04-02 00:03:35 -0700

    Listening to user input

--- react-tutorial-1/src/components/NewExpense/ExpenseForm.js
+++ react-tutorial-1/src/components/NewExpense/ExpenseForm.js
@@ -2,13 +2,17 @@ import "./ExpenseForm.css";
 
 function ExpenseForm() {
 
+    //get current vallue with every keystroke
+    function titleChangeHandler(event) {
+        console.log(event.target.value);
+    };
     return (
         <form>
             <div className="new-expense_controls">
                 {/*input Title*/}
                 <div className="new-expense_control">
                     <label>Title</label>
-                    <input type='text' />
+                    <input type='text' onChange={titleChangeHandler}/>
                 </div>
 
                  {/*input Amount*/}
