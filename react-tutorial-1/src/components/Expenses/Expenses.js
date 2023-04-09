bbe002e5966d81924985b3c602dde92a2bfb53ef
Spidre <52509987+LouisT123@users.noreply.github.com>
2023-04-08 23:50:52 -0700

    render list of data

--- react-tutorial-1/src/components/Expenses/Expenses.js
+++ react-tutorial-1/src/components/Expenses/Expenses.js
@@ -19,39 +19,22 @@ function Expenses(props) {
   }
   return (
     <div>
+      <Card className='expenses'>
       <ExpensesFilter
         currYear={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <Card className="expenses">
-        {/*grabs first title, amount, and date at index [x] for each item
-        from App.js expenses array and sends them into ExpenseItem.*/}
-        <ExpenseItem
-          title1={props.items[0].title}
-          amount1={props.items[0].amount}
-          date1={props.items[0].date}
+       //array of expense items
+      {props.items.map((expense) => (
+        <ExpenseItem 
+          title ={expense.title1}
+          amount={expense.amount1}
+          date={expense.date1}
         />
-
-        <ExpenseItem
-          title1={props.items[1].title}
-          amount1={props.items[1].amount}
-          date1={props.items[1].date}
-        />
-
-        <ExpenseItem
-          title1={props.items[2].title}
-          amount1={props.items[2].amount}
-          date1={props.items[2].date}
-        />
-
-        <ExpenseItem
-          title1={props.items[3].title}
-          amount1={props.items[3].amount}
-          date1={props.items[3].date}
-        />
-      </Card>
+      ))}
+     </Card>
     </div>
   );
-}
+};
 //export to make it usuable outside this file
 export default Expenses;
